6bda44f3de51b487f979512bc7ea116c45dc85a6
NamTuog <hitchenb@students.dsbn.org>
2023-05-15 10:11:15 -0400

    Init Dave Class

--- Classes.py
+++ Classes.py
@@ -1,12 +1,36 @@
 
 
-<<<<<<< HEAD
+
 class Dave():
-    def __init__(self):
-        self.x = #
-        self.y = #
-        self.img =
-        self.rect = pygame.iamge.load('IDLE1.png')
-=======
-Class
->>>>>>> origin/main
+    def __init__(self,x,y):
+        self.x = int(x)
+        self.y = int(y)
+        self.size = (12,12)
+        self.health = 1
+        self.speed = 1
+        self.img = pygame.iamge.load('IDLE1.png')
+        self.img = pygame.transform.scale(self.img,self.size)
+        self.rect = self.img.get_rect()
+        # Directions (only = True when the key is pressed)
+        self.left = False
+        self.right = False
+        self.down = False
+        # Movement of the player. Is added to the X/Y coordinates after clicking.
+        self.movement_x = 0
+        self.movement_y = 0
+        # Jumping
+        self.up = False
+        self.jump = False
+        self.jump_speed = 25
+
+    def move(self):    #including jump
+
+
+    def fall(self):
+
+
+
+class Robot():
+
+
+class Alien():
