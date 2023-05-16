46b9504df0aa716dfab4bbab3778ccbbf80a9ecc
percoa <percoa@students.dsbn.org>
2023-05-16 10:18:25 -0400

    new file made (problem implementing pygame)

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import pygame, sys
 from pygame.locals import QUIT
 
-#The menu
-class menu:
-  def __init__(self):
-    self.image=pygame.image.load("Images/TITLE.png")
-    self.rect = self.image.get_rect()
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
@@ -17,6 +12,3 @@ while True:
             sys.exit()
     pygame.display.update()
 
-    #Starts the menu screen
-    display=menu()
-    DISPLAYSURF.blit(display.image, display.rect)
