f9cb47f4f3ca07a6e424bdd08d0623a68b685b89
NamTuog <hitchenb@students.dsbn.org>
2023-05-18 10:17:08 -0400

    Work on items

--- Classes/Alien.py
+++ Classes/Alien.py
@@ -1,11 +1,18 @@
 
 class Alien():
+<<<<<<< HEAD
     def __init__(self,lx,rx,wany):
+=======
+    def __init__(self):
+>>>>>>> 3741446 (Work on items)
         self.size = (20,20)
         self.speed = 5
         self.img = pygame.image.load('WALK2.png')
         self.img = pygame.transform.scale(self.img, self.size)
         self.rect = self.img.get_rect()
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3741446 (Work on items)
     def wander(self):
