5ae7381b5cda65be67c6ec66bf062c958b2330fd
NamTuog <hitchenb@students.dsbn.org>
2023-05-23 10:09:12 -0400

    Create Spike & add ability to take damage

--- Classes/Dave.py
+++ Classes/Dave.py
@@ -1,5 +1,5 @@
 import pygame, time
-
+import Items, Spike
 
 class Dave():
     def __init__(self, x, y):
@@ -151,7 +151,7 @@ class Dave():
                         self.health = 1
                         subBoost = False
 
-    def checkHit(self, spikes, enemies):
+    def checkHit(self):
         if self.health == 0:
             die()
         for spike in spikes():
