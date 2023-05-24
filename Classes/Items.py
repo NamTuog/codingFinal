57f6692f1d4bf3ecd6360e60fcebc9c5cea42b24
NamTuog <hitchenb@students.dsbn.org>
2023-05-24 09:52:27 -0400

    Preparing to code first level

--- Classes/Items.py
+++ Classes/Items.py
@@ -1,40 +1,40 @@
-import pygame
+import pygame, Images
 
 items = []
-class SpeedUp(): # Item gives player a speed up at tho cost of a jump down
+class SpeedUp: # Item gives player a speed up at tho cost of a jump down
     def __init__(self,x,y): # Creates self and takes spawn point
         self.x = x
         self.y = y
         self.size = (5,5)
-        self.img = pygame.image.load('SpeedUp.png')
+        self.img = pygame.image.load('SPEEDBOOSTITEM.png')
         self.img = pygame.transform.scale(self.img, self.size)
         self.rect = self.img.get_rect()
-        self.rect.move(x, y)
+        self.rect.move(self.x, self.y)
     def collectSpeed(self):
         self.kill
 
-class JumpUp(): # Item gives player a jump up at tho cost of a speed down
-    def __init__(self,x,y):
+class JumpUp: # Item gives player a jump up at tho cost of a speed down
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.size = (5,5)
-        self.img = pygame.image.load('JumpUp.png')
+        self.img = pygame.image.load('JUMPBOOSTITEM.png')
         self.img = pygame.transform.scale(self.img, self.size)
         self.rect = self.img.get_rect()
-        self.rect.move(x, y)
+        self.rect.move(self.x, self.y)
 
     def collectJump(self):
             self.kill()
 
 class Sub(): # Grants the player temporary immortality
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.size = (5,5)
         self.img = pygame.image.load('Sub.png')
         self.img = pygame.transform.scale(self.img, self.size)
         self.rect = self.img.get_rect()
-        self.rect.move(x,y)
+        self.rect.move(self.x, self.y)
 
     def collectSub(self):
         self.kill
