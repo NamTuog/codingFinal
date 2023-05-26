23ddd121c9025baa55c498e08be9a63a67635403
percoa <percoa@students.dsbn.org>
2023-05-26 09:26:21 -0400

    Tried fixing up level 1 file a bit

--- Level1.py
+++ Level1.py
@@ -1,31 +1,19 @@
-import pygame
-import Dave, Spike, Alien
+Import pygame
+import Spike, Alien, Robot, Dave
 
-class Lv1():
-    def __init__(self):
-        self.image = pygame.image.load("Images/WORLD1BACKGROUND.png")
-        self.image = pygame.transform.scale(self.image, (800, 600))
-        self.rect = self.image.get_rect()
+def init_1():   # Creates and moves all assests for the first level
+    spikes = ()
+    enemies = ()
+    Dave.rect = Dave.rect.move(125,10)
+    spike = Spike.Spike(200, 10)
+    spikes.append(spike)
+    spike = Spike.Spike(210,10)
+    spikes.append(spike)
+    spike = Spike.Spike(220,10)
+    spikes.append(spike)
+    spike = Spike.Spike(230,10)
+    spikes.append(spike)
+    alien = Alien.Alien(240,10)
+    enemies.append(alien)
 
-    def init_1(self):   # Creates and moves all assests for the first level
-        spikes = ()
-        enemies = ()
-        Dave.rect = Dave.rect.move(125,10)
-        spike = Spike.Spike(200, 10)
-        spikes.append(spike)
-        spike = Spike.Spike(210,10)
-        spikes.append(spike)
-        spike = Spike.Spike(220,10)
-        spikes.append(spike)
-        spike = Spike.Spike(230,10)
-        spikes.append(spike)
-        alien = Alien.Alien(240,10)
-        enemies.append(alien)
-
-    def update(self,enemies,spikes):
-        SCREEN.blit(self.image, self.rect)
-        for enemy in enemies:
-            SCREEN.blit(enemy.img,enemy.rect)
-        for spike in spikes:
-            SCREEN.blit(SPIKE.png, spike.rect)
-        SCREEN.blit(Dave.rect)
+def
