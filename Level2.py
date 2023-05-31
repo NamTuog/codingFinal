7f5ff1a149803918507263bdc15f894ae64287af
NamTuog <hitchenb@students.dsbn.org>
2023-05-31 10:00:39 -0400

    Files for all levels

---
+++ Level2.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Lv1():
+    def __init__(self):
+        self.image = pygame.image.load("Images/WORLD1BACKGROUND.png")
+        self.image = pygame.transform.scale(self.image, (800, 600))
+        self.rect = self.image.get_rect()
+
+    def init_1(self,Dave,Spike,Alien):   # Creates and moves all assests for the first level
+        spikes = []
+        enemies = []
+        Dave = Dave.Dave(125,10)
+        Dave.rect = Dave.rect.move(125,10)
+        spike = Spike.Spike(200, 10)
+        spikes.append(spike)
+        spike = Spike.Spike(210,10)
+        spikes.append(spike)
+        spike = Spike.Spike(220,10)
+        spikes.append(spike)
+        spike = Spike.Spike(230,10)
+        spikes.append(spike)
+        alien = Alien.Alien(240,10,10)
+        enemies.append(alien)
+        return spikes, enemies, Dave
+
+def update(Lv1,enemies,spikes,Dave,SCREEN,Alien):
+    SCREEN.blit(Lv1.image, Lv1.rect)
+    for alien in enemies:
+        SCREEN.blit(alien.img, alien.rect)
+    for spike in spikes:
+        SCREEN.blit(spike.img, spike.rect)
+    SCREEN.blit(Dave.image,Dave.rect)
